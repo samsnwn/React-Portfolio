9c7369eaa3e519e7d6cc013ea08ddaa550a2dd67
samsnwn <samuel.c.glantz@gmail.com>
2022-06-23 13:10:00 +0200

    added styles

--- src/components/ConfirmMessage.jsx
+++ src/components/ConfirmMessage.jsx
@@ -9,8 +9,8 @@ export default function ConfirmMessage(props) {
 
   return (
     <div className="contact-container" style={{height: '40vh', display: 'flex', justifyContent: 'space-evenly', flexDirection: 'column'}}>
-        <h2>Thanks {props.userInputData.name} for sending me a message about {props.userInputData.subject}!</h2>
-        <h2>I will get back to you soon </h2>
+        <h1 className="contact-message">Thanks <span>{props.userInputData.name}</span> for sending me a message about <span>{props.userInputData.subject}</span>!</h1>
+        <p className="confirm">I will get back to you soon </p>
         <button type='button' className="btn-gradient-border" onClick={returnToForm} style={{fontSize: '20px' , borderRadius: '15px', padding: '8px',  cursor: 'pointer'}}>Send another message</button>
     </div>
   )
